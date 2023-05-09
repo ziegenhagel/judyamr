7128d2ebce559d4e14a51348809e893d4f4c19d6
Dominik <dominik.ziegenhagel@gmail.com>
2023-05-09 10:09:23 +0200

    load all from file

--- server/api/getraenke.ts
+++ server/api/getraenke.ts
@@ -1,29 +1,31 @@
 import * as fs from "fs";
 
-let getranke = `
-Kategorie;Name;Kurze Inhaltsangabe;Zubereitung (ganz knapp)
-Alkoholfrei;Apfelschorle;Apfelsaft mit Mineralwasser;1:1 Apfelsaft/Mineralwasser mischen
-Alkoholfrei;Ananassaft;Mit frischer Ananas;Ananas schälen, in Stücke schneiden, pürieren
-Alkoholfrei;Orangensaft;Frischer Orangensaft;Orangen auspressen
-Alkoholfrei;Ananasschorle;Ananassaft mit Mineralwasser;1:1 Ananassaft/Mineralwasser mischen
-Alkoholfrei;Spezi;Cola mit Orangenlimonade;1:1 Cola/Orangenlimonade mischen
-Alkoholfrei;Coca Cola;Cola; Cola servieren
-Alkoholfrei;Eistee;Tee, Zitrone, Zucker;Tee aufgießen, Zitrone und Zucker hinzufügen
-Alkoholisch;Bier;Verschiedene Sorten;
-Alkoholisch;Wein;Rotwein, Weißwein;
-Alkoholisch;Cuba Libre; Rum und Coke;Rum und Coke mischen
-Alkoholisch;Gin Tonic;Gin, Tonic, Limette;Gin, Tonic, Limette mischen
-Alkoholisch;Whiskey Sour;Whiskey, Zitronensaft,Zucker; Whiskey, Zitronensaft, Zucker mischen
-Alkoholisch;Aperol Spritz;Aperol, Prosecco, Soda;Aperol, Prosecco, Soda mischen,  Eis
-Alkoholisch;Sekt;Verschiedene Sorten; Sekt servieren
-Alkoholisch;Pina Colada;Rum, Ananassaft,Kokoscreme;Zutaten mixen, Eis
-Alkoholisch;Lord Voldemort;Vodka, blue curaÁao, Lemon squash und Sprite;Zutaten mischen, Eis
-Koffeinhaltig;Kaffee;Filterkaffee;Kaffee zubereiten und servieren
-Koffeinhaltig;Cappuccino;Espresso,Milchschaum;Espresso, Milchschaum hinzu geben
-Koffeinhaltig;Espresso;Starker Kaffee;Espresso zubereiten und servieren
-Koffeinhaltig;Schwarztee;Schwarzteeblätter;Tee aufgießen & ziehen lassen
-`
+// let getranke = `
+// Kategorie;Name;Kurze Inhaltsangabe;Zubereitung (ganz knapp)
+// Alkoholfrei;Apfelschorle;Apfelsaft mit Mineralwasser;1:1 Apfelsaft/Mineralwasser mischen
+// Alkoholfrei;Ananassaft;Mit frischer Ananas;Ananas schälen, in Stücke schneiden, pürieren
+// Alkoholfrei;Orangensaft;Frischer Orangensaft;Orangen auspressen
+// Alkoholfrei;Ananasschorle;Ananassaft mit Mineralwasser;1:1 Ananassaft/Mineralwasser mischen
+// Alkoholfrei;Spezi;Cola mit Orangenlimonade;1:1 Cola/Orangenlimonade mischen
+// Alkoholfrei;Coca Cola;Cola; Cola servieren
+// Alkoholfrei;Eistee;Tee, Zitrone, Zucker;Tee aufgießen, Zitrone und Zucker hinzufügen
+// Alkoholisch;Bier;Verschiedene Sorten;
+// Alkoholisch;Wein;Rotwein, Weißwein;
+// Alkoholisch;Cuba Libre; Rum und Coke;Rum und Coke mischen
+// Alkoholisch;Gin Tonic;Gin, Tonic, Limette;Gin, Tonic, Limette mischen
+// Alkoholisch;Whiskey Sour;Whiskey, Zitronensaft,Zucker; Whiskey, Zitronensaft, Zucker mischen
+// Alkoholisch;Aperol Spritz;Aperol, Prosecco, Soda;Aperol, Prosecco, Soda mischen,  Eis
+// Alkoholisch;Sekt;Verschiedene Sorten; Sekt servieren
+// Alkoholisch;Pina Colada;Rum, Ananassaft,Kokoscreme;Zutaten mixen, Eis
+// Alkoholisch;Lord Voldemort;Vodka, blue curaÁao, Lemon squash und Sprite;Zutaten mischen, Eis
+// Koffeinhaltig;Kaffee;Filterkaffee;Kaffee zubereiten und servieren
+// Koffeinhaltig;Cappuccino;Espresso,Milchschaum;Espresso, Milchschaum hinzu geben
+// Koffeinhaltig;Espresso;Starker Kaffee;Espresso zubereiten und servieren
+// Koffeinhaltig;Schwarztee;Schwarzteeblätter;Tee aufgießen & ziehen lassen
+// `
+//
 
+let getranke = ""
 
 // read from file getraenke.csv
 try {
